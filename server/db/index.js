4c6245eae83aa4495c0f75b08632b8251fa0f777
mohammedismayil <mohammedismayil10@gmail.com>
2021-06-21 22:47:20 +0530

    removed files

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,16 @@
 const mongoose = require('mongoose');
-
+const dotenv = require("dotenv")
+dotenv.config()
 const connectionString = 'mongodb://mongo:27017/reactmern';
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true
 };
-
-mongoose.connect(connectionString, options)
+console.log(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, options)
 .then(() => console.log("Data base connected"))
 .catch((e) => {
+  console.log(process.env.MONGO_URI);
   console.log("Data base not connected");
   console.error('Connection error', e.message);
   
