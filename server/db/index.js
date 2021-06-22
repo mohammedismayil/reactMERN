f53dd6dd1976adea5976d18ea51e838f2c22a8bb
mohammedismayil <mohammedismayil10@gmail.com>
2021-06-22 06:05:48 +0530

    removed files

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://localhost:27017/reactmern';
+const connectionString = 'mongodb://mongodb:27017/reactmern';
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true
