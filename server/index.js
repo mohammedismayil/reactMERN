4d60c3179ca46867e1cc28f6110dc45f443a5f90
mohammedismayil <mohammedismayil10@gmail.com>
2021-06-22 09:04:22 +0530

    removed files

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors())
 app.use(bodyParser.json())
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
+//
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
