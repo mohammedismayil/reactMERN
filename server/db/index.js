2609be6a5a20f775217395ea2c60c183e24d0026
mohammedismayil <mohammedismayil10@gmail.com>
2021-06-26 00:42:49 +0530

    env file changes

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = 'mongodb://127.0.0.1:27017/reactmern'
+const connectionString = 'mongodb://mongo:27017/reactmern'
 const options = {
   useNewUrlParser: true,
   useUnifiedTopology: true 
