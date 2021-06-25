f155a1176148b18a2fd023ffa5ecc75958363f61
mohammedismayil <mohammedismayil10@gmail.com>
2021-06-25 19:39:29 +0530

    mongoooo

--- server/routes/movie-router.js
+++ server/routes/movie-router.js
@@ -9,5 +9,5 @@ router.put('/movie/:id', MovieCtrl.updateMovie)
 router.delete('/movie/:id', MovieCtrl.deleteMovie)
 router.get('/movie/:id', MovieCtrl.getMovieById)
 router.get('/movies', MovieCtrl.getMovies)
-
+// 
 module.exports = router
