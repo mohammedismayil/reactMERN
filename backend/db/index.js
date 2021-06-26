98693713d2bedb2b6397737073212e0f32580985
mohammedismayil <mohammedismayil10@gmail.com>
2021-06-26 12:42:41 +0530

    changed dcoker compose

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,19 +1,32 @@
-const mongoose = require('mongoose');
+/**
+ * Created by Syed Afzal
+ */
+const mongoose = require("mongoose");
 
-const connectionString = 'mongodb://mongo:27017/reactmern'
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true 
+exports.connect = (app) => {
+  const options = {
+    useNewUrlParser: true,
+    autoIndex: false, // Don't build indexes
+    reconnectTries: 30, // Retry up to 30 times
+    reconnectInterval: 500, // Reconnect every 500ms
+    poolSize: 10, // Maintain up to 10 socket connections
+    // If not connected, return errors immediately rather than waiting for reconnect
+    bufferMaxEntries: 0,
+  };
+
+  const connectWithRetry = () => {
+    mongoose.Promise = global.Promise;
+    console.log("MongoDB connection with retry");
+    mongoose
+      .connect(process.env.MONGODB_URI, options)
+      .then(() => {
+        console.log("MongoDB is connected");
+        app.emit("ready");
+      })
+      .catch((err) => {
+        console.log("MongoDB connection unsuccessful, retry after 2 seconds.");
+        setTimeout(connectWithRetry, 2000);
+      });
+  };
+  connectWithRetry();
 };
-mongoose.connect(connectionString, options)
-.then(() => console.log("Data base connected"))
-.catch((e) => {
-  console.log(process.env.MONGO_URI);
-  console.log("Data base not connected");
-  console.error('Connection error', e.message);
-  
-});
-
-const db = mongoose.connection;
-///
-module.exports = db;
