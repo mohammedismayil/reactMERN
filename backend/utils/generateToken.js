ee9b6b3062a9065642fc4f17e3e0942835ccbafd
mohammedismayil <mohammedismayil10@gmail.com>
2021-06-26 21:52:19 +0530

    reverted to default without dockker files

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,7 +1,7 @@
 import jwt from 'jsonwebtoken'
 
 const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+  return jwt.sign({ id }, 'jwtkey', {
     expiresIn: '30d',
   })
 }
