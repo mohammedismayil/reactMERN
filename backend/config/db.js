e647fac3175ff51264c9920ffdb711841ff7c695
mohammedismayil <mohammedismayil10@gmail.com>
2021-06-27 10:03:44 +0530

    added docker files for front end and backend files

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect('mongodb://127.0.0.1:27018/reactmern', {
+    const conn = await mongoose.connect('mongodb://127.0.0.1:27017/reactmern', {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
@@ -11,7 +11,8 @@ const connectDB = async () => {
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
     console.error(`Error: ${error.message}`.red.underline.bold)
-    process.exit(1)
+console.log(" im in  a problem and i wont leave here")
+    // process.exit(1)
   }
 }
 
