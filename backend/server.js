fceefe1970e4c95e2a67d9e5bc6a975238a8d390
mohammedismayil <mohammedismayil10@gmail.com>
2021-06-27 10:45:02 +0530

    added docker files for front end and backend files

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
-import morgan from 'morgan'
+// import morgan from 'morgan'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
@@ -10,7 +10,7 @@ import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
-
+var morgan = require('morgan')
 dotenv.config()
 
 connectDB()
