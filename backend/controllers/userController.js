4d323b369c343de366eaf4cc9d1e94c8384a0b97
mohammedismayil <mohammedismayil10@gmail.com>
2021-07-11 19:22:24 +0530

    added comment

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,7 @@ import User from '../models/userModel.js'
 // @desc    Auth user & get token
 // @route   POST /api/users/login
 // @access  Public
+//
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
 
